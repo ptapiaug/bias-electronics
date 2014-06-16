de3dc40d6c5c0e6741c7c0152b24985465e2ee08
ptapia <tapia@ph1.uni-koeln.de>
2014-06-16 18:40:40 +0200

    Enable/disable relay function

--- avr_src/xmega_test.c
+++ avr_src/xmega_test.c
@@ -88,92 +88,7 @@ int main(void)
 
   while( true )
   {
-    if( got_button )
-    {
-      got_button=false;
-      switch(BUTTON_PORT.IN & BUTTON_MASK)
-      {
-        default:
-          break;
-        case 0x70:
-          sprintf( string, "%s compiled %s, %s",
-                   __FILE__, __DATE__, __TIME__ );
-          RS232_SendString( string );
-          
-          break;
-        case 0Xb0: // set DAC channel c to value v
-          // read channel and value from input string
-          sscanf( (const char *)cmd + 1, "%hhu %"SCNi16"", &c, &v );
-          // set DAC
-          DAC7615_SetOutput( 3, 2045 );
-          // write channel and value to output string
-          sprintf( string, "%d %d", 3, 2045 );
-          // send string via RS232
-          RS232_SendString( string );
-          break;
-        case 0Xd0: // read ADC channel c
-          
-          // write ADC value to output string
-          sprintf( string, "%d", LTC1859_ReadSingleChannel( 7 ) );
-          // send string via RS232
-          RS232_SendString( string );
-          break;
-        case 0Xe0: // sweep DAC, read value, store in SRAM. When done, dump data
-          // show start of measurement on LEDs
-          LED_PORT.OUTSET = 0x09;
-          // prepare SRAM to store data starting from address 0
-          MC23K256_StartSequential( 0, MC23K256_WRITE );
-          // sweep up
-          for ( v = 0; v <= DAC7615_MAXVALUE; v++ )
-          {
-            // set new DAC value
-            DAC7615_SetOutput( 3, v );
-            delay_us(10); // DAC settling time
-            // read ADC and store readout in SRAM
-            MC23K256_SequentialWriteWord( LTC1859_ReadSingleChannel( 7 ) );
-          }
-          // sweep down
-          for ( v = DAC7615_MAXVALUE; v >= 0; v-- )
-          {
-            // set new DAC value
-            DAC7615_SetOutput( 3, v );
-            delay_us(10); // DAC settling time
-            // read ADC and store readout in SRAM
-            MC23K256_SequentialWriteWord( LTC1859_ReadSingleChannel( 7 ) );
-          }
-          MC23K256_StopSequential();
-          // show end of data transfer on LEDs
-          LED_PORT.OUTCLR = 0x09;
-
-          // show start of data transfer on LEDs
-          LED_PORT.OUTSET = 0x06;
-          // prepare SRAM to read data starting from address 0
-          MC23K256_StartSequential( 0, MC23K256_READ );
-          for ( v = 0; v <= 2 * DAC7615_MAXVALUE + 1; v++ )
-          {
-            // read 2 bytes from SRAM and write to string
-            sprintf( string, "%d", MC23K256_SequentialReadWord() );
-            // send string
-            RS232_SendString( string );
-          }
-          MC23K256_StopSequential();
-          // show end of data transfer on LEDs
-          LED_PORT.OUTCLR = 0x06;
-          break;
-      }
-
-    }
-
-    // // check pushbuttons
-    // if ( BUTTON_MASK != ( BUTTON_PORT.IN & BUTTON_MASK ) )
-    // {
-    //   // toggle corresponding LED
-    //   LED_PORT.OUTTGL = ~BUTTON_PORT.IN & BUTTON_MASK;
-    //   // wait for button to be released
-    //   while ( BUTTON_MASK != ( BUTTON_PORT.IN & BUTTON_MASK ) );
-    // }
-
-    // check whether we got a command from RS232
+    
     if ( got_cmd )   // command received
     {
       // clear command number display, maintain pushbutton display
@@ -251,6 +166,22 @@ int main(void)
           // show end of data transfer on LEDs
           LED_PORT.OUTCLR = 0x06;
           i = 4; // command number
+          break;
+        case 'e':
+          PORTA.OUT |= (1 << 4);   // This set 1 in pin4
+          PORTA.OUT &= ~(0 << 5);   // This set 0 in pin5
+          // write channel and value to output string
+          sprintf( string, "Relay enabled");
+          // send string via RS232
+          RS232_SendString( string );
+          break;
+        case 'd':
+          PORTA.OUT |= (1 << 4);  // This set 1 in pin4
+          PORTA.OUT |= (1 << 5);  // This set 1 in pin5
+          // write channel and value to output string
+          sprintf( string, "Relay disabled");
+          // send string via RS232
+          RS232_SendString( string );
           break;
         case 'R': // Reset
           watchdog_init( WDT_PER_8CLK_gc  ); // set watchdog timer to 8 msec
